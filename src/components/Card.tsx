b9b212f6226123c9a2b1d858e59c1b6d74b3be6f
Kaviru(UBUNTU) <kavirunethsara1@outlook.com>
2025-01-18 20:21:08 +0530

    Enhance Card component to accept props for title and description; update CSS for improved styling with border-radius

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,8 @@
-function Card(){
+function Card(props:any) {
     return (
         <div className="card">
-            
+            <h1>{props.title}</h1>
+            <p>{props.discription}</p>
         </div>
     )
 }
