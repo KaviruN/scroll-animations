f7cbbd66b0db141d93accffa1fe13c8c12ce43e5
Kaviru(UBUNTU) <kavirunethsara1@outlook.com>
2025-01-18 18:48:25 +0530

    Refactor HTML structure and enhance CSS transitions for improved element visibility and animation effects

--- script.js
+++ script.js
@@ -5,26 +5,34 @@ gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(TextPlugin);
 
 document.querySelectorAll('.hidden').forEach((el) => {
-  gsap.to(el, {
-    scrollTrigger: {
-      trigger: el,
-      markers: false,
-      scrub: 2,
-      start: 'top 90%',
-    //   end: 'bottom 20%',
-      toggleActions: "restart pause none restart",
-      toggleClass: "show"
-    }
-  });
+    gsap.to(el, {
+        scrollTrigger: {
+            trigger: el,
+            markers: false,
+            scrub: 2,
+            //   start: 'top 90%',
+            //   end: 'bottom 20%',
+            toggleActions: "restart pause none restart",
+            toggleClass: "show"
+        }
+    });
 });
 
 
 gsap.to('.thank', {
     scrollTrigger: {
-      trigger: '.thank',
-      toggleActions: "restart pause none restart",
+        trigger: '.thank',
+        toggleActions: "restart pause none restart",
     },
-    duration: 3,
+    duration: 2,
     text: "Thank you for visiting our website !",
     ease: "none",
-  });
+});
+
+gsap.to('.welcome', {
+    duration: 3,
+    start: 'center center',
+    end: 'bottom 80%',
+    text: "Welcome Kaviru",
+    ease: "none",
+});
